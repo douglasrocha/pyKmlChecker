d9ed09a79e5f512f231ca5b19d45032195e27bef
Douglas Rocha <douglasbellonrocha@gmail.com>
2015-08-16 20:23:50 -0300

    Added kml directory file read instructions

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from urllib import urlopen
+from os import listdir
 from xml.etree import ElementTree
 
 import keytree
 from shapely.geometry import Point, shape
 
+kml_files = listdir("./kml/")
+kml_strings = []
+
+# Reads all kml files to strings' list
+for x in kml_files:
+    myfile = open("./kml/" + x, 'r')
+    kml_strings.append(myfile.read())
+    myfile.close()
+
+print kml_strings[1]
+
 #Parse the KML doc
-doc = urlopen("http://pleiades.stoa.org/places/638753/kml").read()
-tree = ElementTree.fromstring(doc)
-kmlns = tree.tag.split('}')[0][1:]
+for i in range(0, len(kml_strings)):
+    doc = kml_strings[i]
+    curr_file = kml_files[i]
 
-# Find all Polygon elements anywhere in the doc
-elems = tree.findall(".//{%s}Polygon" % kmlns)
+    tree = ElementTree.fromstring(doc)
+    kmlns = tree.tag.split('}')[0][1:]
 
-# Here's our poin of interest
-p = Point(28.722144580890763, 37.707799701548467)
+    # Find all Polygon elements anywhere in the doc
+    elems = tree.findall(".//{%s}Polygon" % kmlns)
 
-# Filter polygon elements using this lambda
-# keytree.geometry() makes a GeoJSON-like
-# geometry object from an element and shape()
-# makes a Shapely object of that
-hits = filter(lambda e: shape(keytree.geometry(e)).contains(p), elems)
+    # Here's our poin of interest
+    p = Point(28.722144580890763, 37.707799701548467)
 
-print hits
+    # Filter polygon elements using this lambda
+    # keytree.geometry() makes a GeoJSON-like
+    # geometry object from an element and shape()
+    # makes a Shapely object of that
+    hits = filter(lambda e: shape(keytree.geometry(e)).contains(p), elems)
+
+    print curr_file + " --> " + str(len(hits))
